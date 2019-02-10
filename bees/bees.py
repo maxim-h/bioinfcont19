04ae6b406ff4522203b58eacd2ace8b0be6f0f60
maxim-h <maksim.holmatov@gmail.com>
2019-02-10 18:45:09 +0300

    bees done (more or less).

--- bees/bees.py
+++ bees/bees.py
@@ -17,31 +17,40 @@ def population(n1, a, b):
 
 def analyzer(n1, a, b):
     diff_1 = n1
+    iterations = 0
     for i, j in population(n1, a, b):
+        iterations += 1
         if i <= 0:
-            return 0
+            return (0, iterations)
         else:
-            diff_2 = i - j
+            diff_2 = abs(i - j)
             if diff_2 > diff_1:
-                return -1
+                return (-1, iterations)
             else:
                 diff_1 = diff_2
                 if diff_1 < err:
-                    return i
+                    return (round(i, 8), iterations)
 
 
 
 
 
-#print(analyzer(0.5, 1, 1))
+# print(analyzer(0.5, 1, 1))
+
+# c = 0
+# for i in population(0.5, 1, 1):
+#     print(i)
+#     if c == 10:
+#         break
+#     c += 1
 
 
-for i in population(0.5, 1, 1):
-    print(i)
 
-
-
-# with open("tests.txt", "r") as t:
-#     n = int(t.readline())
-#     # for i in range(n):
-#     #     print(t.readline())
+with open("tests.txt", "r") as t:
+    with open("results.txt", "w") as res:
+        n = int(t.readline())
+        results = []
+        for i in range(n):
+            n, a, b = map(float, t.readline().strip().split())
+            results.append(str(analyzer(n, a, b)[0]))
+        res.write("\n".join(results))
