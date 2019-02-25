7dc7ad8ed92b62d1704f0477495de968a20bfab6
maxim-h <maksim.holmatov@gmail.com>
2019-02-25 15:06:10 +0300

    Finished easy. Started hard

--- seq_errs/sequencing_errors_hard.py
+++ seq_errs/sequencing_errors_hard.py
@@ -18,6 +18,7 @@ def calling_errors(b: int, p: float):
     if b % 2 == 0:
         yield 0.5 * binomial_pdf(p, b, b/2)
     for i in range(int(floor(b / 2) + 1), b + 1):
+
         yield 1 * binomial_pdf(p, b, i)
 
 
@@ -36,7 +37,7 @@ def error_probability(b: int, l: int, n: int, p: float, k: int):
     if b == 0:
         return cov_prob * 0.75
     else:
-        return cov_prob * error_summ(b, p, 1.0e-7 / cov_prob)
+        return cov_prob * error_summ(b, p, 1.0e-8 / cov_prob)
 
 
 def total_error_probability(l: int, n: int, p: float, k: int):
@@ -45,13 +46,13 @@ def total_error_probability(l: int, n: int, p: float, k: int):
         e = error_probability(cov, l, n, p, k)
         total_error += e
         #print("Total error {} Error {} Coverage {}".format(total_error, e, cov))
-        if e < 1.0e-7:
+        if e < 1.0e-8:
             break
     return total_error
 
 
-with open("1.txt", "r") as tests:
-    with open("results_easy.txt", "w") as res:
+with open("2.txt", "r") as tests:
+    with open("results_hard.txt", "w") as res:
         n = int(tests.readline())
         results = []
         for i in range(n):
